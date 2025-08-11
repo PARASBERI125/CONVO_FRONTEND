24d67d6f812df66d0b12d33070b15564c5e95de7
paras beri <130133581+PARASBERI125@users.noreply.github.com>
2025-08-11 13:14:14 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
-      : "https://convo-backend-nkyh.onrender.com/api",
+      : "https://api.convochat.fun/api",
   withCredentials: true,
 });
